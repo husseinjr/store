db3b8cf3f01dba7a4c80442cdca12d722436dd42
Amr Hussein <amrbp333@gmail.com>
2024-09-08 17:30:14 +0300

    add middleware for rate limiting

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import dotenv from 'dotenv';
 import express, { Application, Request, Response } from 'express';
+import { rateLimit } from 'express-rate-limit';
+
 dotenv.config();
 const port: number = Number(process.env.PORT) || 3000;
 // create instance from the server
 const app: Application = express();
+
+// middlewares
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // time
+    limit: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+  }),
+);
 
 // routing
 app.get('/', (req: Request, res: Response) => {
